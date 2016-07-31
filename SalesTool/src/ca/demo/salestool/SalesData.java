6f3e00d0ed711b981d50213fb6f8dfb4a8ad6822
ThaddeusLincolnStevens <beny1419@yahoo.es>
2016-07-31 23:28:26 +0200

    Completed displaying data in a SalesData object

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -6,8 +6,9 @@ public class SalesData {
 	
 	public void display(){
 		
+		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++){
-			System.out.println("");
+			System.out.println("Next value: " + data[i]);
 		}
 	}
 
